0e6c0afce450b69928215a4efee303335869c1ae
Cristhyan D. Marchena <demarchenac@uninorte.edu.co>
2022-10-31 03:13:10 -0500

    Test: Signature

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello world + TailwindCSS!</h1>
+    <h1 className="text-3xl font-bold underline">Hello world + TailwindCSS</h1>
   );
 }
 
