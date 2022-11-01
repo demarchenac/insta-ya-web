39dc6ac316e90d0504554b71d352caf46e3025b4
Cristhyan D. Marchena <demarchenac@uninorte.edu.co>
2022-11-01 00:08:55 -0500

    Chore: Add git hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">Hello world + TailwindCSS</h1>
-  );
+	return (
+		<h1 className="text-3xl font-bold underline">Hello world + TailwindCSS</h1>
+	);
 }
 
 export default App;
