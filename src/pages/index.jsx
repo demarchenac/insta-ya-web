7a2d268b2c1d93b08e88f5d135b97dc591c8b31b
Cristhyan D. Marchena <demarchenac@uninorte.edu.co>
2022-11-03 01:10:14 -0500

    WIP: Enable '/client/requests' path

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,9 @@
-import { Navigate, Outlet, Route } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { SignIn, action as signInAction } from './auth/SignIn';
 import { SignUp, action as signUpAction } from './auth/SignUp';
 import { Requests } from './client/Requests';
 
-export function RootLayout() {
+function RootLayout() {
 	return (
 		<main className="w-screen h-screen bg-gray-100">
 			<Outlet />
@@ -25,26 +25,17 @@ export const appRoutes = [
 					{ path: 'sign-up', element: <SignUp />, action: signUpAction },
 				],
 			},
+			{
+				path: 'client',
+				children: [
+					{ index: true, element: <Navigate to="/client/requests" replace /> },
+					{
+						path: 'requests',
+						element: <Outlet />,
+						children: [{ index: true, element: <Requests /> }],
+					},
+				],
+			},
 		],
 	},
 ];
-
-export function AppRoutes() {
-	return (
-		<>
-			<Route path="/">
-				<Route index element={<Navigate to="/auth/sign-in" replace />} />
-
-				{/* Auth routes */}
-				<Route path="/auth/sign-in" element={<SignIn />} />
-				<Route path="/auth/sign-up" element={<SignUp />} />
-
-				{/* Client routes */}
-				<Route path="client/requests" element={<Requests />} />
-
-				{/* Fallback */}
-				<Route path="*" element={<Navigate to="/auth/sign-in" replace />} />
-			</Route>
-		</>
-	);
-}
